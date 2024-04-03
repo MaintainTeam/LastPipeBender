9472c69e70bdca0092528619c1b1ae6333d7d980
evermind <evermind@tuxfamily.org>
2024-04-03 22:56:08 +0200

    BraveNewPipeLegacy: Move setting HttpsURLConnection default ssl factory to BraveApp

--- app/src/braveLegacy/java/org/schabi/newpipe/BraveApp.java
+++ app/src/braveLegacy/java/org/schabi/newpipe/BraveApp.java
@@ -1,6 +1,9 @@
 package org.schabi.newpipe;
 
+import android.os.Build;
+
 import org.conscrypt.Conscrypt;
+import org.schabi.newpipe.util.BraveTLSSocketFactory;
 
 import java.security.Security;
 
@@ -11,5 +14,11 @@ public class BraveApp extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         Security.insertProviderAt(Conscrypt.newProvider(), 1);
+
+        // enable TLS1.2/1.3 for <=kitkat devices, to fix download and play for
+        // media.ccc.de, rumble, soundcloud, peertube sources
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.KITKAT) {
+            BraveTLSSocketFactory.setAsDefault();
+        }
     }
 }
